4b79ac12a67b015c29731e1db5c645434427d1fb
xiao <xiao@localhost>
2019-10-14 20:51:50 -0300

    the very first test, for import

---
+++ src/main.py
@@ -0,0 +1,9 @@
+
+
+# read line from input.txt
+
+# separate line into list
+
+# find last str of list
+
+# cast above into int
--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,4 +1,6 @@
 import pytest
 
-def test_dummy():
-    assert 2 + 2 == 4
+from src import main
+
+def test_import_builtin_libraries_module1():
+    assert 'os' in dir(main), 'Have you imported the built-in `os` library?'
