5a8a278d43750114bdc11dd87a4c501eee1d1bb4
xiao <xiao@localhost>
2019-10-15 19:23:53 -0300

    copy utils.py from Python-Baseball; use it in a unit test

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,6 +1,9 @@
 import pytest
 
 from src import main
+from .utils import get_assignments, get_calls, get_for_loops
 
-def test_import_builtin_libraries_module1():
-    assert 'os' in dir(main), 'Have you imported the built-in `os` library?'
+
+def test_get_call_with_open_module1():
+    assert 'open:input.txt' in get_calls(
+        main), 'Are you calling `open()` and pass in the `input.txt` file?'
