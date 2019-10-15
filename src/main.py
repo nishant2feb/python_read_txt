bd54b81e7c2554a45ece0bf48b5321d3212e5bf4
xiao <xiao@localhost>
2019-10-14 21:02:39 -0300

    initial working source code and input file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 
-
 # read line from input.txt
+with open('input.txt') as infile:
 
-# separate line into list
+    # loop through each line
+    for line in infile:
+        # separate line into list
+        splitted = line.split(' ')
 
-# find last str of list
+        # find last str of list
+        last = splitted[-1]
 
-# cast above into int
+        # cast above into int
+        value = int(last)
+
+        # print out the value
+        print(value)
