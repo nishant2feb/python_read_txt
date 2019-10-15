ad4c77095d45aabbe49de3f4087edc1293886931
xiao <xiao@localhost>
2019-10-15 20:51:02 -0300

    update requirements.txt; add test_use_astunparse()

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,6 +1,11 @@
+import ast
+import inspect
+
+import astunparse
 import pytest
 
 from src import main
+
 from .utils import get_assignments, get_calls, get_for_loops
 
 
@@ -19,3 +24,28 @@ def test_get_loop_for_each_line():
     iter_id = loops[0]['iter:id']
     assert target_id == 'line', 'Do you have a variable `line` in the loop?'
     assert iter_id == 'infile', 'Do you loop through the `infile`?'
+
+
+def test_split_line_with_space():
+    calls = get_calls(main)
+    message = "Are you calling `split()` of the line variable and pass in a space ' ' as the separator?"
+    assert 'line:split: ' in calls, message
+
+
+def test_get_last_element_in_splitted():
+    assignments = get_assignments(main, return_type='list')
+
+    # print(assignments)
+
+
+def test_use_astunparse():
+    inspected = inspect.getsource(main)
+    parsed = ast.parse(inspected)
+    unparsed = astunparse.unparse(parsed)
+
+    print(type(inspected))  # 'str'
+    print(type(parsed))
+    print(type(unparsed))
+
+    print(inspected)
+    
